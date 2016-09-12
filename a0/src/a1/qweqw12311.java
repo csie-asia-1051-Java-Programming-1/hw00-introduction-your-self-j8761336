0527fa979f0ff6df2910deaec83da992a18cf95e
j8761336 <105021026@live.asia.edu.tw>
2016-09-12 12:52:47 +0800

    20160912 hw00-1
    hw00-1

---
+++ a0/src/a1/qweqw12311.java
@@ -0,0 +1,13 @@
+package a1;
+
+public class qweqw12311 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("I'mdandan\n");
+		System.out.print("Asia University \nCSIE 1A");
+		}
+}
